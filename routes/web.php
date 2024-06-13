1bc1ab77abe4a93a08956f603f5a93fd50263c62
aditya <danirama282@gmail.com>
2024-06-13 09:24:20 +0700

    route changes

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/myprofile', function () {
